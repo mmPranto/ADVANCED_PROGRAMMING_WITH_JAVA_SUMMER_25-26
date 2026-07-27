cf61e02815ce61de2e43ce8f4e98002fcbd26a46
mmPranto <zminhazul29@gmail.com>
2026-07-28 00:34:51 +0600

    nested class3 uploaded

---
+++ NestedClass3/src/Outer.java
@@ -0,0 +1,15 @@
+public class Outer {
+    int outer_x=100;
+
+    void test(){
+        for(int i=0;i<10;i++){
+            class Inner{
+                void display(){
+                    System.out.println("diplay: outer_x = "+outer_x);
+                }
+            }
+            Inner inner = new Inner();
+            inner.display();
+        }
+    }
+}
