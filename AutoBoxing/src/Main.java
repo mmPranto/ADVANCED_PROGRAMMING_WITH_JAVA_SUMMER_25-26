27a7419360b9243d77e87b74c957cf86178b209e
mmPranto <zminhazul29@gmail.com>
2026-07-30 23:44:19 +0600

    Auto Boxing and Unbox

---
+++ AutoBoxing/src/Main.java
@@ -0,0 +1,18 @@
+public class Main{
+    public static void main(String[] args){
+        //autoboxing, unboxing
+
+        //primitive -> wrapper = autoboxing
+
+        int b= 10;
+        Integer a = b;      //     Integer.valueOf(b);
+
+
+        Integer c = 20;
+        int d = c;//unbox
+
+        Integer f = null;
+        int e = f;
+
+    }
+}
